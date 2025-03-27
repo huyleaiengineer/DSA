adf48f2b4e6c4b00ad20649b5ce88e10ba97dcdb
Huy <lenhathuy.aiengineer@gmail.com>
2025-03-27 07:43:27 +0700

    Add List problems and creating array

---
+++ 1.C++/1_Create_random_int_arr.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/// parameters
+const int MAX_ARR_LEN = 10000;
+int a[MAX_ARR_LEN]; // Array
+int n; // Number of array elements
+
+/// List of functions
+void showarr(int a[], int n);
+
+int main()
+{
+    int min_value, max_value; // Min, max value of array elemnents
+    cout << "Enter number of array elements: ";
+    cin >> n;
+    do{
+        cout << "Enter max value of array elements: ";
+        cin >> max_value;
+        cout << "Enter min value of array elements: ";
+        cin >> min_value;
+        if (max_value < min_value)
+            cout << "Max must be greater than min value, re-enter!\n";
+    } while (max_value < min_value);
+
+    srand(time(nullptr));
+    cout << "\n------------\nCreated Array: \n";
+    for (int i = 0; i < n; ++i)
+        a[i] = rand()%(max_value-min_value+1)+min_value;
+    // show arr
+    showarr(a, n);
+}
+
+void showarr(int a[], int n)
+{
+    for (int i = 0; i < n; ++i)
+        cout << a[i] << ' ';
+    cout << endl;
+}
